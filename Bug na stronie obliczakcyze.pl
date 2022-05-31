c64dbea2369702211ef682d6d0eb00b3fe6d79b2
Joanna Kraciuk <103620609+JoannaKraciuk@users.noreply.github.com>
2022-05-31 10:05:01 +0200

    Create Bug na stronie obliczakcyze.pl

---
+++ Bug na stronie obliczakcyze.pl
@@ -0,0 +1,27 @@
+W artykule "Rejestracja auta z NIemiec" nie działa link do listy tłumaczy przysięgłych
+
+Kroki:
+
+Otworzyć przeglądarkę Mozilla Firefox
+
+Wejść na stronę https://www.obliczakcyze.pl/ 
+
+Na dole strony kliknąć “Rejsetracja auta z Niemiec”
+
+W artykule “Tłumaczenie dokumentów” kliknąć link do listy tłumaczy przysięgłych
+
+Po kliknięciu wyświatla się strona “Nie udało się nawiązać bezpiecznego połączenia”
+
+Po kliknięciu “Więcej informacji…” ponownie wyświetla się strona https://support.mozilla.org/pl/kb/nie-uda%C5%82o-si%C4%99-nawiaza%C4%87-bezpiecznego-po%C5%82%C4%85czenia?as=u&utm_source=inproduct 
+
+Oczekiwany rezultat:
+
+Przejście do strony z listą tłumaczy przysięgłych.
+
+Aktualny rezultat:
+
+Wyświetla się strona “Nie udało się nawiązać bezpiecznego połączenia”.
+
+Środowisko
+
+Windows 11 Mozilla 99.0.1
